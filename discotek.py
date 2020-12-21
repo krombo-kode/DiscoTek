75855216a9f177e653c039116f658e264b0b52c8
Krombo-Kode <pklose75@gmail.com>
2020-12-20 20:03:07 -0600

    Updated discotek.py to add functionality

--- discotek.py
+++ discotek.py
@@ -1,30 +1,55 @@
 from mutagen.easyid3 import EasyID3
-# import eyed3
+import eyed3
 import sys
-from os import rename, listdir
-from os.path import isfile, join
+from os import rename, mkdir, getcwd, walk
+from os.path import isfile, isdir, join
 
-def rename_track(input_file):
+
+def rename_track(input_file, output_directory):
     tags = EasyID3(input_file)
     extension = str(input_file)[-4:]
-    new_filename = tags['title'][0] + " - " + tags['artist'][0] + extension
-    rename(input_file, new_filename)
+    title = tags['title'][0]
+    artist = tags['artist'][0]
+    new_filename = title + " - " + artist + extension
+    destination_path = output_directory + "\\" + artist.title()
+    if not isdir(destination_path):
+        mkdir(destination_path)
+    destination = destination_path +"\\"+ new_filename
+    rename(input_file, destination)
 
 
 def generate_track_list(path):
-    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-    onlyfiles.remove('discotek.py')
-    return onlyfiles
+    onlytracks = []
+    for dirpath, subdirList, fileList in walk(path):
+        for x in fileList:
+            if x.endswith(".mp3"):
+                onlytracks.append(join(dirpath, x))
+    if len(onlytracks) == 0:
+        print("No tracks found!")
+    return onlytracks
+
+
+def create_output_library():
+    directory = "DiscoTek Library"
+    parent_dir = getcwd()
+    path = join(parent_dir, directory)
+    if not isdir(path):
+        mkdir(path)
+    else:
+        print("Existing DiscoTek Library Found!")
+    return path
 
 
 def main(path):
     track_queue = generate_track_list(path)
+    output_directory = create_output_library() + "\\"
     for track in track_queue:
         try:
-            rename_track(track)
+            rename_track(track, output_directory)
         except:
+            print(f"{track} has bad tags, trying to find the right ones!")
             continue
 
 
+print(getcwd())
 main('./')
-print("Done.")
