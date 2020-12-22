c1e63e38dcd4d6f3d224242f11375b9e4d286c1d
Krombo-Kode <pklose75@gmail.com>
2020-12-22 13:00:08 -0600

    updated to v0.2, updated readme

--- discotek.py
+++ discotek.py
@@ -1,5 +1,4 @@
 from mutagen.easyid3 import EasyID3
-import eyed3
 import sys
 from os import rename, mkdir, getcwd, walk
 from os.path import isfile, isdir, join
@@ -14,8 +13,11 @@ def rename_track(input_file, output_directory):
     destination_path = output_directory + "\\" + artist.title()
     if not isdir(destination_path):
         mkdir(destination_path)
-    destination = destination_path +"\\"+ new_filename
-    rename(input_file, destination)
+    destination = destination_path + "\\" + new_filename
+    if isfile(destination):
+        print(f'{new_filename} already exists in the DiscoTek Library!')
+    else:
+        rename(input_file, destination)
 
 
 def generate_track_list(path):
@@ -40,15 +42,62 @@ def create_output_library():
     return path
 
 
-def main(path):
-    track_queue = generate_track_list(path)
-    output_directory = create_output_library() + "\\"
+def process_tracks(track_queue, output_directory):
+    failed_tracks = []
     for track in track_queue:
         try:
             rename_track(track, output_directory)
         except:
-            print(f"{track} has bad tags, trying to find the right ones!")
+            print(f"{track} has bad tags!")
+            failed_tracks.append(track)
             continue
+    return failed_tracks
+
+
+def bad_track_prompt(bad_tag_tracks):
+    print(
+        f'Found {len(bad_tag_tracks)} tracks with bad tags. Do you want to try to fix them?')
+    response = input("Y/N?> ").upper()
+    if response == "Y":
+        for track in bad_tag_tracks:
+            print(track)
+        print("This feature is not yet implimented")
+        return
+    elif response == "N":
+        return False
+    else:
+        print("Please input Y or N.")
+        bad_track_prompt(bad_tag_tracks)
+
+
+def process_bad_tag_tracks(bad_tag_tracks, output_directory):
+    for track in bad_tag_tracks:
+        found_tags = tag_finder(track)
+        # Should return a dict of tags
+        repaired_track = tag_fixer(track, found_tags)
+        # Add tags to track
+        process_tracks([repaired_track], output_directory)
+
+
+def tag_finder(track):
+    # Will use AudD to find track, AudioDB to generate tags, return tags as dict
+    return track
+
+
+def tag_fixer(track, tags):
+    # will read from tag_finder dict and apply tags to track
+    return {}
+
+
+def main(path):
+    track_queue = generate_track_list(path)
+    output_directory = create_output_library() + "\\"
+    bad_tag_tracks = process_tracks(track_queue, output_directory)
+    if bad_track_prompt(bad_tag_tracks):
+        process_bad_tag_tracks(bad_tag_tracks, output_directory)
+    else:
+        pass
+    print("Complete!")
 
 
 main('./')
